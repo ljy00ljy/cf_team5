64760038e82074416b0e5ade381938030ddd0396
kanghanil <khi8305@gmail.com>
2016-11-30 16:39:36 +0900

    전체

--- src/team5/helper/SMTPAuthenticator.java
+++ src/team5/helper/SMTPAuthenticator.java
@@ -13,6 +13,7 @@ public class SMTPAuthenticator extends Authenticator {
 	@Override
     public PasswordAuthentication getPasswordAuthentication() {
 		// 자신의 Gmail 메일 주소와 비밀번호 입력.
-        return new PasswordAuthentication("abel8479@gmail.com", "dlwndudljy");
+        return new PasswordAuthentication("khi8305@gmail.com", "45335184qq");
     }
 }
+
